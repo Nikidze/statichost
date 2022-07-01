5fb23ffebc90c95281ecdba4bb8aff795d7b0919
Сидоров Никита <42678036+Nikidze@users.noreply.github.com>
2022-07-01 12:56:24 +0300

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
       if(window.mobileCheck()){
         location.replace('vk://vk.com/nikidze_dev')
         setTimeout(() => {
-          window.open('https://vk.com/nikidze_dev', '_blank').focus();
+          location.replace('https://vk.com/nikidze_dev')
         }, 1000);
       }else{
         window.open('https://vk.com/nikidze_dev', '_blank').focus();
