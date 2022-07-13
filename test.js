8d93451ad3b923c65162b9bc8603abca708039b3
Сидоров Никита <42678036+Nikidze@users.noreply.github.com>
2022-07-13 21:36:19 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
     function goToVk(){
       if(window.mobileCheck()){
-        location.replace('vk://vk.com/nikidze_dev')
+        location.replace('vk://vk.com/cakeshop.school')
         setTimeout(() => {
-          location.replace('https://vk.com/nikidze_dev')
+          location.replace('https://vk.com/cakeshop.school')
         }, 1000);
       }else{
-        window.open('https://vk.com/nikidze_dev', '_blank').focus();
+        window.open('https://vk.com/cakeshop.school', '_blank').focus();
       }
     }
